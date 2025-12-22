7801dace5aa6b706d0feaae23d8998526068b0ae
OliWan9 <oliver.hitz.7@gmail.com>
2025-12-22 20:50:50 +0100

    Imports, Print CPU, Disk, Memory JSON response

--- go.mod
+++ go.mod
@@ -1,1 +1,11 @@
 module SystemMonitor
+
+go 1.25.5
+
+require (
+	github.com/ebitengine/purego v0.9.1 // indirect
+	github.com/shirou/gopsutil/v4 v4.25.11 // indirect
+	github.com/tklauser/go-sysconf v0.3.16 // indirect
+	github.com/tklauser/numcpus v0.11.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/shirou/gopsutil/v4/cpu"
+	"github.com/shirou/gopsutil/v4/disk"
+	"github.com/shirou/gopsutil/v4/mem"
+)
+
+func main() {
+	memUsage, _ := mem.VirtualMemory()
+	diskUsage, _ := disk.Usage("/")
+	cpuUsage, _ := cpu.Info()
+
+	fmt.Println("CPU: ", cpuUsage)
+	fmt.Println("Disk: ", diskUsage)
+	fmt.Println("Memory: ", memUsage)
 }
